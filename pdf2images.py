457afc8b383a343566da9319fdbeeb7c596804a4
ufolux <richadlu001@gmail.com>
2023-06-01 13:51:58 -0400

    wip: compress images

--- pdf2images.py
+++ pdf2images.py
@@ -2,34 +2,22 @@
 # coding=utf-8
 
 import os
-import argparse
+import sys
+from PIL import Image
 from pdf2image import convert_from_path
 
-def convert_pdf_to_images(pdf_path, output_folder):
-    # 调用convert_from_path函数将PDF转换为图像
-    images = convert_from_path(pdf_path)
+def main(input_pdf, output_folder):
+    # 1. 使用 pdf2image 库将 PDF 转换为图片
+    images = convert_from_path(input_pdf)
 
-    # 创建图像输出文件夹
-    os.makedirs(output_folder, exist_ok=True)
+    # 2. 使用 PIL 库将图片压缩并保存到输出文件夹
+    for i, img in enumerate(images):
+        img.thumbnail((1024, 1024))  # 可以根据需要修改这个大小
+        img.save(os.path.join(output_folder, f'image_{i}.jpg'), format='JPEG', quality=60)
 
-    # 保存每个图像到输出文件夹
-    for i, image in enumerate(images):
-        image_path = os.path.join(output_folder, f'page_{i + 1}.jpg')
-        image.save(image_path, 'JPEG')
-
-        print('保存图像：', image_path)
-
-if __name__ == '__main__':
-    # 创建解析器对象
-    parser = argparse.ArgumentParser(description='Convert PDF to images')
-
-    # 添加参数
-    parser.add_argument('pdf', help='PDF file path')
-    parser.add_argument('output', help='output folder path')
-
-    # 解析命令行参数
-    args = parser.parse_args()
-
-    # 调用函数将PDF转换为图像
-    convert_pdf_to_images(args.pdf, args.output)
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py [input_pdf] [output_folder]")
+        sys.exit(1)
+    main(sys.argv[1], sys.argv[2])
 
